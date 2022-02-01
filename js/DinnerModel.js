48bfef6283db794f20da866e74e95065e93f6a46
cristi <cristi@kth.se>
2022-02-01 12:10:42 +0100

    machine name changed

--- js/DinnerModel.js
+++ js/DinnerModel.js
@@ -3,44 +3,44 @@ class DinnerModel {
     
     /* Note the default value of function parameters */
     constructor(guestsParam/* FIXME default value 2 */, dishesParam=[] ) {
-	// note that you always need to use "this." when you refer to an object property!
-	this.guests= guestsParam;  
-	
-	this.dishes= dishesParam.slice(0);  // we store a copy (clone) of dishesParam.
-	// JS objects (like arrays such as dishesParam) are sent as function parameters by reference not by value.
-	// Therefore we are getting a reference to the dishesParam array, and anybody can still modify the array content!
-	// So we clone dishesParam to ensure that our DinnerModel object is *not* affected if the dishesParam array content is changed after the call to new DinnerModel(num, dishesParam),
+        // note that you always need to use "this." when you refer to an object property!
+        this.guests= guestsParam;  
+        
+        this.dishes= dishesParam.slice(0);  // we store a copy (clone) of dishesParam.
+        // JS objects (like arrays such as dishesParam) are sent as function parameters by reference not by value.
+        // Therefore we are getting a reference to the dishesParam array, and anybody can still modify the array content!
+        // So we clone dishesParam to ensure that our DinnerModel object is *not* affected if the dishesParam array content is changed after the call to new DinnerModel(num, dishesParam),
 
-	// Week 2: cloning can be also achieved using spread syntax (...) like so:  this.dishes= [...dishesParam];
-	// or this.dishes=new Array(...dishesParam)
+        // Week 2: cloning can be also achieved using spread syntax (...) like so:  this.dishes= [...dishesParam];
+        // or this.dishes=new Array(...dishesParam)
     }
     
     /* Set the number of guests to the given value. 
        The value must be a positive integer, throw an Error otherwise.
      */
     setNumberOfGuests(num) {
-	// TODO throw an error with the message
-	//          "number of guests not a positive integer"
-	// if num is not a positive integer. Then:
-	// this.guests= TODO ;
+        // TODO throw an error with the message
+        //          "number of guests not a positive integer"
+        // if num is not a positive integer. Then:
+        // this.guests= TODO ;
     }
 
     /* Return the number of guests */ 
     getNumberOfGuests() {
-	// return this.  TODO 
+        // return this.  TODO 
     }
     
     /* Return all the dishes on the menu. 
      */
     getMenu() {
-	//TODO 
+        //TODO 
     }
     
     /* add a dish to the menu. If a dish with the same type already exists, remove it. 
      The dish object can be obtained as a result of DishSource.getDishDetails() or DishSource.searchDishes() 
     */
     addToMenu(dish) {
-	//TODO  
+        //TODO  
     }
     
     /* Remove dish from the menu. Identify the dish by its id. Both of the following should work:
@@ -48,23 +48,23 @@ class DinnerModel {
        model.removeFromMenu({id:3})
      */
     removeFromMenu(dish) {
-	//TODO 
+        //TODO 
     }
 
     /* Return the dish of the given type from the menu, or undefined */
     getDishOfType(type){
-	//TODO 
+        //TODO 
     }
     
     /* Week 2: Utility method do compute a dish price depending on its ingredient prices and quantities.
      This method could be declared static as it does not depend on the DinnerModel data */
     getDishPrice(dish){
-	//TODO 
+        //TODO 
     }
 
     /* Week 2: Total price for the dinner given the number of guests */
     getDinnerPrice(){
-	//TODO 
+        //TODO 
     }    
     /* Week 2: Return an array of ingredients for the DinnerModel dishes, 
        with each ingredient showing up maximum once, and the quantities added up.
@@ -75,17 +75,17 @@ class DinnerModel {
        Advanced (bonus) : implement functionally without defining a const, e.g. return dishes.reduce(TODO collect all ingredients in an array).reduce(TODO group ingredients by name in an object)...
     */
     getIngredients(){
-	// to make sure we have one entry for each ingredient name, the suitable data structure is a Dictionary,
-	// with ingredient names as keys
-	// All JavaScript objects are dictionaries, so we use an object called combinedIngredients to collect ingredient data. combinedIngredients[name] will return the ingredient object with the respective name
-	const combinedIngredients={};
-	
-	// TODO  for each dish (this.dishes.forEach() ), for each dish ingredient, set  combinedIngredients[name] to
-	// 1) a copy of the ingredient object if combinedIngredients[name] is falsy, i.e. we have not encoutered this ingredient yet during the forEach iterations
-	// 2) a copy of the ingredient object with an increased amount if combinedIngredients[name] is truthy, i.e. we have encountered this ingredient before
-	// functional code uses expressions rarhter than statements so use a ternary expression ? :  to distinguish between case (1) and (2)
-	
-	return /*TODO now we don't need the keys any longer, we just need an array of ingredients. Find the appropriate Object method for that */
+        // to make sure we have one entry for each ingredient name, the suitable data structure is a Dictionary,
+        // with ingredient names as keys
+        // All JavaScript objects are dictionaries, so we use an object called combinedIngredients to collect ingredient data. combinedIngredients[name] will return the ingredient object with the respective name
+        const combinedIngredients={};
+        
+        // TODO  for each dish (this.dishes.forEach() ), for each dish ingredient, set  combinedIngredients[name] to
+        // 1) a copy of the ingredient object if combinedIngredients[name] is falsy, i.e. we have not encoutered this ingredient yet during the forEach iterations
+        // 2) a copy of the ingredient object with an increased amount if combinedIngredients[name] is truthy, i.e. we have encountered this ingredient before
+        // functional code uses expressions rarhter than statements so use a ternary expression ? :  to distinguish between case (1) and (2)
+        
+        return /*TODO now we don't need the keys any longer, we just need an array of ingredients. Find the appropriate Object method for that */
     }
 }
 
@@ -97,7 +97,7 @@ class DinnerModel {
 const  DishSource={
     /* Returns a dish of specific ID */
     getDishDetails(id) {
-	//TODO 
+        //TODO 
     },
 
     /* 
@@ -113,12 +113,12 @@ const  DishSource={
        DishSource.searchDishes({})  returns all dishes
     */
     searchDishes(searchParams) {
-	//TODO 
+        //TODO 
     },
 
     /* Week 2: Retrieve a dish asynhronously by returning a Promise.
        Read the dishes via fetch from this URL, which returns dishes in JSON format:
-       http://sunset.nada.kth.se:8080/iprog/file?DM1595/dishes.json
+       http://standup.csc.kth.se:8080/iprog/file?DM1595/dishes.json
        Then filter for the dish with the respective ID. 
        Example use:
            DishSource.getDishDetailsPromise(2).then(dish=>console.log(dish))
@@ -131,7 +131,7 @@ const  DishSource={
       Optional-advanced: implement the promise rejection fully functionally (no statements), using e.g. a promise constructor  new Promise((resolve, reject)=> fetch(...)....then(TODO using the resolve and reject callbacks in e.g. a ?: ternary expression)
     */
     getDishDetailsPromise(id) {
-	return fetch(/*TODO*/).then(/*TODO*/).then(/*TODO*/);
+        return fetch(/*TODO*/).then(/*TODO*/).then(/*TODO*/);
     },    // extra comma is legal in object properties
 };  /* good to have a semicolon after a let or const declaration */
 
